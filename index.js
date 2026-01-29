deee1b8f8d1441c215ee539460ded9b210a002d1
unknown <muktinathrajbanshi786@gmail.com>
2026-01-29 09:08:39 +0545

    wah hoki kiya ho haina hola ki ho

--- index.js
+++ index.js
@@ -1,9 +1,37 @@
 const setOfWords = [
-    "Moy name is muktinath rajbanshi and I am a youtuber.",
+    "My name is muktinath rajbanshi and I am a youtuber.",
     "Hope you are having fun this is a simple game you can make.",
     "If you want the source code then link is given in the github so you can create your own version of this challenge."
 ];
 
 const msg = document.getElementById("msg");
-const mywords = document.getElementById("mywords");
+const typeWords = document.getElementById("mywords");
 const btn = document.getElementById("btn");
+let startTime, endTime;
+
+const playGame = () => {
+    let randomNumber = Math.floor(Math.random() * setOfWords.length);
+    msg.innerText = setOfWords[randomNumber];
+    let date = new Date();
+    startTime = date.getTime();
+    btn.innerText = "Done";
+}
+
+const endPlay = () => {
+    let date = new Date();
+    endTime = date.getTime();
+    let totalTime = ((endTime - startTime)/ 1000);
+    console.log(totalTime);
+    
+}
+
+btn.addEventListener("click", function() {
+    if(this.innerText == "Start"){
+        typeWords.disabled = false;
+        playGame();
+    } else if (this.innerText == "Done") {
+        typeWords.disabled = true;
+        btn.innerText = "Start";
+        endPlay();
+    }
+})
