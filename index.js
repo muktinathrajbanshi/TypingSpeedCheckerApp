411326a95c98ff87ccd5637b601acc16c332d021
unknown <muktinathrajbanshi786@gmail.com>
2026-01-29 08:07:03 +0545

    now let's get started with logic building part

---
+++ index.js
@@ -0,0 +1,9 @@
+const setOfWords = [
+    "Moy name is muktinath rajbanshi and I am a youtuber.",
+    "Hope you are having fun this is a simple game you can make.",
+    "If you want the source code then link is given in the github so you can create your own version of this challenge."
+];
+
+const msg = document.getElementById("msg");
+const mywords = document.getElementById("mywords");
+const btn = document.getElementById("btn");
